734c1e47f4cdec311b0e93b3cd9440029c445392
Sébastien Gaudard <43199628+SebGrd@users.noreply.github.com>
2021-03-22 04:19:03 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const {Client} = require('discord.js');
+const client = new Client();
+
+const User = require('./commands/user')
+const Scores = require('./commands/scores')
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity('/osu help', {type: 'COMPETING'})
+});
+
+client.on('message', msg => {
+    const [prefix, command, ...param] = msg.content.split(' ');
+    if (prefix === '/osu') {
+        if (command === 'user') User(msg, param);
+        if (command === 'scores') Scores(msg, param);
+    }
+});
+
+client.login(process.env.DISCORD_KEY).catch(err => console.log(err))
